4160aff97b2fc7d73a67ac93f3b99467eea2b5c8
suhang <798247300@qq.com>
2020-10-28 14:34:34 +0800

    antd-m init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPlugins = require('next-compose-plugins');
+const withLess = require('@zeit/next-less');
 const withCSS = require('@zeit/next-css');
 const withImages = require('next-images');
 const withFonts = require('next-fonts');
@@ -9,6 +10,16 @@ const withTM = require('next-transpile-modules')([
   'react-native-status-bar-height',
   'react-native-size-matters',
 ]); // pass the modules you would like to see transpiled
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cssLoaderGetLocalIdent = require('css-loader/lib/getLocalIdent.js');
+const fs = require('fs');
+const path = require('path');
+const lessToJS = require('less-vars-to-js');
+
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+);
 
 module.exports = withPlugins(
   [
@@ -18,6 +29,22 @@ module.exports = withPlugins(
         cssModules: true,
         cssLoaderOptions: {
           localIdentName: '[path]___[local]___[hash:base64:5]',
+          getLocalIdent: (context, localIdentName, localName, options) => {
+            const hz = context.resourcePath.replace(context.rootContext, '');
+            if (/node_modules/.test(hz)) {
+              return localName;
+            }
+            return cssLoaderGetLocalIdent(context, localIdentName, localName, options);
+          },
+        },
+      },
+    ],
+    [
+      withLess,
+      {
+        lessLoaderOptions: {
+          javascriptEnabled: true,
+          modifyVars: themeVariables, // make your antd custom effective
         },
       },
     ],
@@ -26,7 +53,29 @@ module.exports = withPlugins(
     withFonts,
   ],
   {
-    webpack: (config) => {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd-mobile\/.*?\/style.*?/;
+        const origExternals = [...config.externals];
+        // eslint-disable-next-line no-param-reassign
+        config.externals = [
+          // eslint-disable-next-line consistent-return
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback();
+            if (typeof origExternals[0] === 'function') {
+              origExternals[0](context, request, callback);
+            } else {
+              callback();
+            }
+          },
+          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+        ];
+
+        config.module.rules.unshift({
+          test: antStyles,
+          use: 'null-loader',
+        });
+      }
       config.resolve.alias = {
         ...(config.resolve.alias || {}),
         // Transform all direct `react-native` imports to `react-native-web`
--- src/pages/counter/index.tsx
+++ src/pages/counter/index.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'antd-mobile';
+import { Text } from 'react-native-elements';
+
 import { decrement, increment, incrementByAmount, incrementAsync, selectCount } from './slice';
-// import styles from './counter.module.css';
+import styles from './counter.module.less';
 
 function Counter() {
   const count = useSelector(selectCount);
@@ -11,10 +14,11 @@ function Counter() {
   return (
     <div>
       <div>
-        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+        <Button type="primary" aria-label="Increment value" onClick={() => dispatch(increment())}>
           +
-        </button>
-        <span>{count}</span>
+        </Button>
+        <span className={styles.val}>{count}</span>
+        <Text h2>{count}</Text>
         <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
           -
         </button>
@@ -25,12 +29,12 @@ function Counter() {
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
-        <button onClick={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}>
+        <Button onClick={() => dispatch(incrementByAmount(Number(incrementAmount) || 0))}>
           Add Amount
-        </button>
-        <button onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}>
+        </Button>
+        <Button onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}>
           Add Async
-        </button>
+        </Button>
       </div>
     </div>
   );
